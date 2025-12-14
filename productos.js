f61bbe460b77af618fdbf0adbc30d9cf849ab9f3
DanyValue <DDMSoporteyventas@gmail.com>
2025-12-13 20:26:09 -0600

    Update productos.js

--- productos.js
+++ productos.js
@@ -3,12 +3,12 @@ const productos = [
     id: 1,
     nombre: "Careta Protector Facial Rígido Uso Rudo Industrial Y Medica Color Amarillo",
     precio: 126,
-    imagen: "img/productos/CaretaProtectorFacial_1.png",
+    imagen: "./img/productos/CaretaProtectorFacial_1.png",
     descripcionCorta: "Careta Protector Facial",
     descripcionLarga: "Careta Protector Facial Rígido Uso Rudo Industrial Y Medica Color Amarillo",
     imagenes: [
-      "img/productos/CaretaProtectorFacial_1.png",
-      "img/productos/CaretaProtectorFacial_2.png"
+      "./img/productos/CaretaProtectorFacial_1.png",
+      "./img/productos/CaretaProtectorFacial_2.png"
     ],
     video: ""
   },
@@ -16,11 +16,11 @@ const productos = [
     id: 2,
     nombre: "Conector Borne 1/2 Para Soldadora",
     precio: 345.80,
-    imagen: "img/productos/ConectorBorne_1.png",
+    imagen: "./img/productos/ConectorBorne_1.png",
     descripcionCorta: "Conector Borne 1/2",
     descripcionLarga: "Conector Borne 1/2 Para Soldadora",
     imagenes: [
-      "img/productos/ConectorBorne_1.png"
+      "./img/productos/ConectorBorne_1.png"
     ],
     video: ""
   },
@@ -29,11 +29,11 @@ const productos = [
     id: 3,
     nombre: "Interruptor Apagador Soldadora Switch Inversor Laston Infra",
     precio: 67.58,
-    imagen: "img/productos/InterruptorApagador_1.png",
+    imagen: "./img/productos/InterruptorApagador_1.png",
     descripcionCorta: "Interruptor Apagador",
     descripcionLarga: "Interruptor Apagador Soldadora Switch Inversor Laston Infra",
     imagenes: [
-      "img/productos/InterruptorApagador_1.png"
+      "./img/productos/InterruptorApagador_1.png"
     ],
     video: ""
   }
