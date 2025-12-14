4bf31c042406e1689f90f786f42f4382771e261e
DanyValue <DDMSoporteyventas@gmail.com>
2025-12-13 19:12:39 -0600

    Create productos.js

---
+++ productos.js
@@ -0,0 +1,27 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Soldadora Inverter 200A",
+    precio: 3500,
+    imagen: "img/soldadora1.jpg",
+    descripcionCorta: "Soldadora compacta y potente",
+    descripcionLarga: "Soldadora inverter de 200 amperes, ideal para trabajos profesionales y domésticos. Bajo consumo y excelente rendimiento.",
+    imagenes: [
+      "img/soldadora1.jpg",
+      "img/soldadora1_2.jpg"
+    ],
+    video: ""
+  },
+  {
+    id: 2,
+    nombre: "Careta Automática",
+    precio: 1200,
+    imagen: "img/careta.jpg",
+    descripcionCorta: "Protección automática",
+    descripcionLarga: "Careta automática con oscurecimiento rápido, ideal para soldadores profesionales.",
+    imagenes: [
+      "img/careta.jpg"
+    ],
+    video: ""
+  }
+];
