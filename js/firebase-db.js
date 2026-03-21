ee13afdd7e07349d6014eb39362fadad8d045f56
DanyValue <DDMSoporteyventas@gmail.com>
2026-03-20 23:22:48 -0600

    Implement Firestore functions for products
    Added functions to add and retrieve products from Firestore.

--- js/firebase-db.js
+++ js/firebase-db.js
@@ -1,1 +1,23 @@
+import { app } from "./firebase-config.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+const db = getFirestore(app);
+
+// Guardar producto
+export async function agregarProducto(producto) {
+  await addDoc(collection(db, "productos"), producto);
+}
+
+// Obtener productos
+export async function obtenerProductos() {
+  const snapshot = await getDocs(collection(db, "productos"));
+  return snapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data()
+  }));
+}
