fb4006a4baab0633b4774290d5a6fbb56734fc12
DanyValue <DDMSoporteyventas@gmail.com>
2026-03-21 00:50:12 -0600

    Update firebase-db.js

--- js/firebase-db.js
+++ js/firebase-db.js
@@ -1,30 +1,13 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { app } from "./firebase-config.js";
 import {
-  getFirestore,
   collection,
   addDoc,
-  getDocs
+  getDocs,
+  getFirestore
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// 🔥 TU CONFIG REAL
-const firebaseConfig = {
-  apiKey: "AIzaSyDzrtSDv8wXwj2hoWZ7SBTkm1gKt-1q0A4",
-  authDomain: "gen-lang-client-0177915830.firebaseapp.com",
-  projectId: "gen-lang-client-0177915830",
-  storageBucket: "gen-lang-client-0177915830.firebasestorage.app",
-  messagingSenderId: "8073771450",
-  appId: "1:8073771450:web:ac974ac82aae6969b70583"
-};
-
-// 🔥 Inicializar Firebase AQUÍ directamente
-const app = initializeApp(firebaseConfig);
-import { initializeFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-const db = initializeFirestore(app, {
-  experimentalForceLongPolling: true
-});
-
-// ================= FUNCIONES =================
+// 🔥 IMPORTANTE: usar ESTA instancia
+const db = getFirestore(app);
 
 export async function agregarProducto(producto) {
   console.log("🔥 Intentando guardar en Firebase...");
